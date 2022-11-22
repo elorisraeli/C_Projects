6f6bac2dc080d4779c7d64b403adf13e06cc7452
Elor Israeli <elorisraeli@gmail.com>
2022-11-22 09:14:01 +0200

    Fix output

--- main.c
+++ main.c
@@ -8,37 +8,36 @@ int main(){
     printf("Enter start and end range (with space between them): ");
     scanf("%d %d", &start, &end);
 
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = start; i <= end; i++)
     {
         if (isArmstrong(i)){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     
-    printf("\nThe Palindrome numbers are: ");
+    printf("\nThe Palindrome numbers are:");
     for (int i = start; i <= end; i++)
     {
         if (isPalindrome(i)){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     
-    printf("\nThe Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for (int i = start; i <= end; i++)
     {
         if (isPrime(i)){
-            printf("%d ", i);
+            printf("%d", i);
         }
     }
     
-    printf("\nThe Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for (int i = start; i <= end; i++)
     {
         if (isStrong(i)){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nDone.\n");
     return 0;
 }
